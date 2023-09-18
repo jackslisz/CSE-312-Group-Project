0ff21a9500b6e6337ca9093410cb8d66134412b0
Jack Slisz <jackslis@buffalo.edu>
2023-09-18 18:23:43 -0400

    Comments

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+#Importing Flask framework and render_template module
 from flask import Flask, render_template
 
+#Creating Flask app instance and storing it in app
+#__name__ holds the name of current Python module
 app = Flask(__name__)
 
+#Decorator to turn Python function home_page into Flask view function
 @app.route("/")
 def home_page():
+    #Calling render_template to look and open the file index.html
     return render_template('index.html')
 
+#Checking if __name__ is the name of top-level environment of program
 if __name__ == "__main__":
+    #If so, calling run (on app) on the local host and port 8080
     app.run(host='0.0.0.0', port='8080')
--- dockerfile
+++ dockerfile
@@ -1,8 +1,11 @@
+#Taken from lecture:
 FROM python:3.8
 ENV HOME /root
 WORKDIR /root
 COPY . .
-# Download dependancies
+#Download dependancies
 RUN pip3 install -r requirements.txt
+#Exposing the correct port
 EXPOSE 8080
+#Opening app.py unbuffered
 CMD python3 -u app.py
