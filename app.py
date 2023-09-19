9ab16128ff658e4105344f0fde6af3c797820f4f
Jack Slisz <jackslis@buffalo.edu>
2023-09-18 20:31:23 -0400

    Working app without JS

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from flask import Flask, render_template
 #__name__ holds the name of current Python module
 app = Flask(__name__)
 
-#Decorator to turn Python function home_page into Flask view function
+#Decorators to turn Python function home_page into Flask view function
 @app.route("/")
+@app.route("/home")
 def home_page():
-    #Calling render_template to look and open the file index.html
+    #Calling render_template to look for and open the file index.html
     return render_template('index.html')
 
 #Checking if __name__ is the name of top-level environment of program
