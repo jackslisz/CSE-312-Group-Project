6e6a8a8108b48492ab4a06118eb307883b0bfdd7
Jack Slisz <jackslis@buffalo.edu>
2023-09-21 12:24:42 -0400

    Final Save

--- app.py
+++ app.py
@@ -19,41 +19,41 @@ def home_page():
     #Returning the finished response
     return response
 
-# #Decorator to turn Python function style_page into Flask view function
-# @app.route('/static/css/<path:file_path>')
-# def style_page(file_path):
-#     #Calling send_from_directory to edit headers and MIME types of non-HTML files
-#     response = send_from_directory('static', file_path)
-#     #Setting the nosniff header
-#     response.headers['X-Content-Type-Options'] = 'nosniff'
-#     #Setting the correct MIME type for CSS
-#     response.headers['Content-Type'] = 'text/css; charset=utf-8'    
-#     #Returning the finished response
-#     return response
+#Decorator to turn Python function style_page into Flask view function
+@app.route('/static/css/<path:file_path>')
+def style_page(file_path):
+    #Calling send_from_directory to edit nosniff header of non-HTML files
+    response = send_from_directory('static/css/', file_path)
+    #Setting the nosniff header
+    response.headers['X-Content-Type-Options'] = 'nosniff' 
+    #Setting the correct MIME type for CSS
+    response.headers['Content-Type'] = 'text/css; charset=utf-8'    
+    #Returning the finished response
+    return response
 
-# #Decorator to turn Python function script_page into Flask view function
-# @app.route('/static/js/<path:file_path>')
-# def script_page(file_path):
-#     #Calling send_from_directory to edit headers and MIME types of non-HTML files
-#     response = send_from_directory('static', file_path)
-#     #Setting the nosniff header
-#     response.headers['X-Content-Type-Options'] = 'nosniff'
-#     #Setting the correct MIME type for JS
-#     response.headers['Content-Type'] = 'text/javascript; charset=utf-8'    
-#     #Returning the finished response
-#     return response
+#Decorator to turn Python function script_page into Flask view function
+@app.route('/static/js/<path:file_path>')
+def script_page(file_path):
+    #Calling send_from_directory to edit nosniff header of non-HTML files
+    response = send_from_directory('static/js/', file_path)
+    #Setting the nosniff header
+    response.headers['X-Content-Type-Options'] = 'nosniff' 
+    #Setting the correct MIME type for JavaScript
+    response.headers['Content-Type'] = 'text/javascript; charset=utf-8'    
+    #Returning the finished response
+    return response
 
-# #Decorator to turn Python function image_page into Flask view function
-# @app.route('/static/img/<path:file_path>')
-# def script_page(file_path):
-#     #Calling send_from_directory to edit headers and MIME types of non-HTML files
-#     response = send_from_directory('static', file_path)
-#     #Setting the nosniff header
-#     response.headers['X-Content-Type-Options'] = 'nosniff'
-#     #Setting the correct MIME type for a PNG image
-#     response.headers['Content-Type'] = 'image/png; charset=utf-8'    
-#     #Returning the finished response
-#     return response
+#Decorator to turn Python function image_page into Flask view function
+@app.route('/static/img/<path:file_path>')
+def image_page(file_path):
+    #Calling send_from_directory to edit nosniff header of non-HTML files
+    response = send_from_directory('static/img/', file_path)
+    #Setting the nosniff header
+    response.headers['X-Content-Type-Options'] = 'nosniff' 
+    #Setting the correct MIME type for PNG images
+    response.headers['Content-Type'] = 'image/png'    
+    #Returning the finished response
+    return response
 
 #Decorator to turn Python function visit_counter_cookie into Flask view function
 @app.route("/visit-counter")
@@ -71,7 +71,3 @@ def visit_counter_cookie():
 if __name__ == "__main__":
     #If so, calling run (on app) on the local host and port 8080
     app.run(host='0.0.0.0', port='8080')
-
-# TODO: 
-# Fix Chrome cookie issue
-# Correct Mime Types and nosniff header for all but html
