3f607dd725101ff658cc6c0ce17e0cc8aef7287d
Jack Slisz <jackslis@buffalo.edu>
2023-10-06 19:02:31 -0400

    Updated Front End

---
+++ static/js/functions.js
@@ -0,0 +1,76 @@
+function deleteMessage(messageId) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    request.open("DELETE", "/chat-message/" + messageId);
+    request.send();
+}
+
+function chatMessageHTML(messageJSON) {
+    const username = messageJSON.username;
+    const message = messageJSON.message;
+    const messageId = messageJSON.id;
+    let messageHTML = "<br><button onclick='deleteMessage(" + messageId + ")'>X</button> ";
+    messageHTML += "<span id='message_" + messageId + "'><b>" + username + "</b>: " + message + "</span>";
+    return messageHTML;
+}
+
+function clearChat() {
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML = "";
+}
+
+function addMessageToChat(messageJSON) {
+    const chatMessages = document.getElementById("chat-messages");
+    chatMessages.innerHTML += chatMessageHTML(messageJSON);
+    chatMessages.scrollIntoView(false);
+    chatMessages.scrollTop = chatMessages.scrollHeight - chatMessages.clientHeight;
+}
+
+function sendChat() {
+    const chatTextBox = document.getElementById("chat-text-box");
+    const message = chatTextBox.value;
+    chatTextBox.value = "";
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.response);
+        }
+    }
+    const messageJSON = {"message": message};
+    request.open("POST", "/chat-message");
+    request.send(JSON.stringify(messageJSON));
+    chatTextBox.focus();
+}
+
+function updateChat() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            clearChat();
+            const messages = JSON.parse(this.response);
+            for (const message of messages) {
+                addMessageToChat(message);
+            }
+        }
+    }
+    request.open("GET", "/chat-history");
+    request.send();
+}
+
+function welcome() {
+    document.addEventListener("keypress", function (event) {
+        if (event.code === "Enter") {
+            sendChat();
+        }
+    });
+
+    document.getElementById("paragraph").innerHTML = "<br/>Welcome to CSE312 Social. You're one stop for chat, image sharing, and DMs 🙂";
+    document.getElementById("chat-text-box").focus();
+
+    updateChat();
+    setInterval(updateChat, 2000);
+}
--- static/js/script.js
+++
@@ -1,2 +0,0 @@
-//Changing any element of id use_js to the string below
-document.getElementById('use_js').innerHTML = "Welcome to part 1 of my project! 😀";
